662e32b8e1113d03f719301b3625b449d7fa92eb
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-09 14:05:57 -0300

    feat models class

--- Program.cs
+++ Program.cs
@@ -11,7 +11,16 @@ using (HttpClient cliente = new HttpClient())
         // LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
         // LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
         // LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
-        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+        // LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
+        var musicasPreferidasDoVinicius = new MusicasPreferidas("Daniel");
+        musicasPreferidasDoVinicius.AdicionarMusicasFavoritas(musicas[1]);
+        musicasPreferidasDoVinicius.AdicionarMusicasFavoritas(musicas[6]);
+        musicasPreferidasDoVinicius.AdicionarMusicasFavoritas(musicas[9]);
+        musicasPreferidasDoVinicius.AdicionarMusicasFavoritas(musicas[15]);
+        musicasPreferidasDoVinicius.AdicionarMusicasFavoritas(musicas[71]);
+
+        musicasPreferidasDoVinicius.ExibirMusicasFavoritas();
+        musicasPreferidasDoVinicius.GerarArquivoJson();
     }
     catch (Exception ex)
     {
--- obj/Debug/net8.0/Alura-ScreenSound-pt2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Alura-ScreenSound-pt2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alura-ScreenSound-pt2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3a7c64477a5c9a7f6ff2613de7743c7d663beb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3b463fb3048d1ecb9a24fd0126c20cb3e456cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alura-ScreenSound-pt2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alura-ScreenSound-pt2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
