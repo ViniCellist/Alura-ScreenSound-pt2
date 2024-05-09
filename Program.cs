ce3b463fb3048d1ecb9a24fd0126c20cb3e456cf
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-05-08 22:21:29 -0300

    feat methods

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿using (HttpClient cliente = new HttpClient())
+﻿using System.Text.Json;
+using ScreenSound;
+using ScreenSound.Filtros;
+
+using (HttpClient cliente = new HttpClient())
 {
     try
     {
         string resposta = await cliente.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        // LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        // LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        // LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
+        LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");
     }
     catch (Exception ex)
     {
